19b405e6b5a879dff3c8bb26ed6bcb9389099102
Mariano Perez Chaher <marianopablo97@hotmail.com>
2020-04-23 21:29:14 +0200

    set up csv reader function based on sample from dataset

---
+++ csv_reader.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import csv
+
+filepath = "dataset/dataset_sample.csv"
+
+with open(filepath, mode='r') as dataset:
+    csv_reader = csv.DictReader(dataset)
+    line_count = 0
+
+    for row in csv_reader:
+
+        if line_count == 0:
+            print("first row are headers")
+            line_count += 1
+
+        print("every other row is the data of the 28x28 pixel images")
+        line_count += 1
+    
+    print("Images detected in dataset: "+ str(line_count))
