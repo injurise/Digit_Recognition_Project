2ccbbd3da025c60680082214449a2f84c3737efd
jinjurise <32211854+injurise@users.noreply.github.com>
2020-04-30 01:53:30 +0200

    Update neuralnet.py

--- neuralnet.py
+++ neuralnet.py
@@ -13,13 +13,18 @@
 #Network also works perfectly fine if you just use the first 2 "layer-sets"
 #You can then increase the Kernel size to 3,3 again. My accuracy for that was 0.987 which seemed decent
 
+#maybe try a batch regulizar like this kernel_regularizer=regularizers.l2(0.01) in e.g. Conv2
+
+# maybe try adding data augmentation on train data before
+
+#If you want to look at the data plt.imshow() can be used
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, Dropout,MaxPooling2D
-
 
 
 img_rows, img_cols = 28, 28
@@ -55,26 +60,21 @@ model = Sequential()
 model.add(Conv2D(20, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=(img_rows, img_cols, 1)))
-model.add(Conv2D(20, kernel_size=(2, 2), activation='relu'))
-model.add(Conv2D(20, kernel_size=(2, 2), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2),strides = 2))
+model.add(Conv2D(20, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(20, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(3, 3),strides = 2))
 model.add(Dropout(0.5))
 
-model.add(Conv2D(20, kernel_size=(2, 2), activation='relu'))
-model.add(Conv2D(20, kernel_size=(2, 2), activation='relu'))
+model.add(Conv2D(20, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(20, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2),strides = 2))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(20, kernel_size=(2, 2), activation='relu'))
-model.add(Conv2D(20, kernel_size=(2, 2), activation='relu'))
-model.add(Conv2D(20, kernel_size=(2, 2), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2),strides = 2))
-model.add(Dropout(0.25))
+
 
 model.add(Flatten())
 
-model.add(Dense(128, activation='relu'))
-model.add(Dense(128, activation='relu'))
+model.add(Dense(128, activation='relu', ))
 model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer='adam',
@@ -82,10 +82,18 @@ model.compile(loss=keras.losses.categorical_crossentropy,
 
 
 
-model.fit(x, y,
+hist = model.fit(x, y,
           batch_size=128,
-          epochs=6,
+          epochs=2,
           validation_split = 0.1)
+
+# nice plotting of accuracy and  validation acc on epochs
+plt.plot(hist.history['accuracy'], label='train acc')
+plt.plot(hist.history['val_accuracy'], label='validation acc')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+
 prediction = model.predict(x_test)
 final_pred = prediction.argmax(axis = 1)
 submission = np.zeros((28000,2))
